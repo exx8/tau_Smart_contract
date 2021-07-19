608407836b3ff905d03b00db2b5f5710ab980226
exx8 <exx8eran@gmail.com>
2021-07-20 01:02:46 +0300

    add generic ether call

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+import {addBattle} from "./solidity/Web3Scripts/frontend";
+import {sendInvitation} from "./Mail";
+
+declare let window: any;
+
 export function getAnchor() {
     if (window.location.hash) {
         var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
@@ -5,5 +10,27 @@ export function getAnchor() {
         // hash found
     } else {
         return "";
+    }
+}
+
+export async function genericEtherRequest(customRequest: (string) => any) {
+    if (window.ethereum) {
+        try {
+            let address = await window.ethereum.enable();
+            customRequest(address);
+            l
+
+        } catch (e) {
+            console.log('Payment using Metamask  was denied');
+
+        }
+    } else if (window.web3) {
+        console.log("Need to see how to extract address in this case, provider is just window.web3. than, call addBattle");
+        console.log(window.web3)
+
+
+    } else {
+        console.log('please install a wallet. recommended: Metamask');
+
     }
 }
