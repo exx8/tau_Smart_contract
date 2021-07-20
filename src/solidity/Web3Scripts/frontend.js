522128522f34cecfeeacdc06f66b16770d694f12
exx8 <exx8eran@gmail.com>
2021-07-20 22:23:08 +0300

    removed debugger

--- src/solidity/Web3Scripts/frontend.js
+++ src/solidity/Web3Scripts/frontend.js
@@ -85,7 +85,6 @@ export const withdraw= async function (identifier,provide,from = address) {
 	result=await contract.getPastEvents('MyEvent',{filter:{id: identifier},fromBlock: res-2, toBlock: res}); // we filter by id
 	const winner=result[result.length-1].returnValues.win;
 	let return_msg=null;
-	debugger;
 	if (winner===0){
 	    return_msg='You lost ' +result[0].returnValues.amount+' in battle: '+identifier;
     }
