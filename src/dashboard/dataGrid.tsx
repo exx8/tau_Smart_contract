9c9801fbf5fccf7bbf09dfd34915ec873b7c0283
exx8 <exx8eran@gmail.com>
2021-07-28 20:06:13 +0300

    update table according to data

--- src/dashboard/dataGrid.tsx
+++ src/dashboard/dataGrid.tsx
@@ -1,47 +1,64 @@
 import React from "react";
 import ReactDataGrid from '@inovua/reactdatagrid-community';
 import '@inovua/reactdatagrid-community/index.css';
+import {fillEtherDetailsInFunc, genericEtherRequest} from "../utils";
+import {getAll as getAllFE} from "../solidity/Web3Scripts/frontend";
+import {addBattleResult} from "../BattleMenu";
+import {TrendingDown, TrendingUp} from "@material-ui/icons";
 
 
-export class DataGrid extends React.Component<{}, {}> {
-     columns = [
-        { name: 'amountBet', header: 'amount', minWidth: 50, defaultFlex: 2 },
-         { name: 'betType', header: 'type', minWidth: 50, defaultFlex: 3 },
-         { name: 'isUp', header: 'trend', minWidth: 50, defaultFlex: 4 },
-         { name: 'WhoWin', header: 'status', minWidth: 50, defaultFlex: 5 },
+export class DataGrid extends React.Component<{}, {dataSource:any[]}> {
+    columns = [
+        {name: 'amountBet', header: 'wei', minWidth: 20, defaultFlex: 2},
+        {name: 'betType', header: 'type', minWidth: 40, defaultFlex: 3},
+        {name: 'isUp', header: 'trend', minWidth: 10, defaultFlex: 2.2},
+        {name: 'WhoWin', header: 'status', minWidth: 10, defaultFlex: 3},
 
-         { name: 'betDate', header: 'date', maxWidth: 1000, defaultFlex: 1 },
+        {name: 'betDate', header: 'date', defaultFlex: 6},
     ];
 
-     gridStyle = { minHeight: 550,display:"inline-block" ,minWidth:"20vw"};
+    gridStyle = {minHeight: 250, display: "inline-block", minWidth: "35rem"};
 
-     dataSource = [
-        { id: 1, name: 'John McQueen', age: 35 },
-        { id: 2, name: 'Mary Stones', age: 25 },
-        { id: 3, name: 'Robert Fil', age: 27 },
-        { id: 4, name: 'Roger Robson', age: 81 },
-        { id: 5, name: 'Billary Konwik', age: 18 },
-        { id: 6, name: 'Bob Martin', age: 18 },
-        { id: 7, name: 'Matthew Richardson', age: 54 },
-        { id: 8, name: 'Ritchie Peterson', age: 54 },
-        { id: 9, name: 'Bryan Martin', age: 40 },
-        { id: 10, name: 'Mark Martin', age: 44 },
-        { id: 11, name: 'Michelle Sebastian', age: 24 },
-        { id: 12, name: 'Michelle Sullivan', age: 61 },
-        { id: 13, name: 'Jordan Bike', age: 16 },
-        { id: 14, name: 'Nelson Ford', age: 34 },
-        { id: 15, name: 'Tim Cheap', age: 3 },
-        { id: 16, name: 'Robert Carlson', age: 31 },
-        { id: 17, name: 'Johny Perterson', age: 40 },
-    ];
+    state = {dataSource: []}
 
-    render()
-    {
-      return   <ReactDataGrid
+    render() {
+        return <ReactDataGrid
             idProperty="id"
             columns={this.columns}
-            dataSource={this.dataSource}
+            dataSource={this.state.dataSource}
             style={this.gridStyle}
         />
+    }
+
+    async componentDidMount() {
+        let getAll = await fillEtherDetailsInFunc(getAllFE);
+        genericEtherRequest(async (address) => {
+            let results: addBattleResult = await getAll();
+            console.log(results);
+            const status={
+                0:"opponent",
+                1: "creator",
+                2: "draw",
+                3:"not settled"
+            }
+            let dataSource = results.map((value, index, array) => {
+
+                    let returned_value = {
+                        amountBet: value.amountBet,
+                        betType: String(value.betType),
+                        isUp: (value.isUp)? <TrendingUp/>:<TrendingDown/>,
+                        WhoWin: status[value.whoWin as 1|2|3|0 ],
+                        betDate: new Date(value.betDate).toString()
+
+                    };
+                    return returned_value;
+                }
+            );
+            console.log(dataSource)
+            this.setState({
+                dataSource: dataSource
+            });
+        })
+
     }
 }
