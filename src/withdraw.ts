92a0e6b89f34c7ef640c2fc925777491745fcfe9
exx8 <exx8eran@gmail.com>
2021-07-29 14:58:51 +0300

    fixed withdraw

--- src/withdraw.ts
+++ src/withdraw.ts
@@ -1,6 +1,6 @@
 import {fillEtherDetailsInFunc} from "./utils";
-
+import {withdraw as withdrawFE} from "./solidity/Web3Scripts/frontend";
 export async function withdraw() {
-    let withdrawFunc = await fillEtherDetailsInFunc(withdraw);
-    return withdrawFunc();
+    let withdrawFunc = await fillEtherDetailsInFunc(withdrawFE);
+     withdrawFunc();
 }
