56d8585b4ae138664bb7d846c499c4dff1c1c983
exx8 <exx8eran@gmail.com>
2021-07-29 14:17:30 +0300

    remove unused.

--- src/withdraw.ts
+++ src/withdraw.ts
@@ -1,7 +1,5 @@
-import {getAll} from "./solidity/Web3Scripts/frontend";
-import {fillEtherDetailsInFunc, genericEtherRequest} from "./utils";
-import {addBattleResult} from "./BattleMenu";
-import {withdraw as frontend_withdraw} from "./solidity/Web3Scripts/frontend.js"
+import {fillEtherDetailsInFunc} from "./utils";
+
 export async function withdraw() {
     let withdraw = await fillEtherDetailsInFunc(withdraw);
     return withdraw();
