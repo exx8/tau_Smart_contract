f1caadd04a0d45627a414e114588522aa85629b8
Admin <hochstadt@mail.tau.ac.il>
2021-07-30 12:27:03 +0300

    Renaming
    (cherry picked from commit 5d49b41d47676659fcf0619545845848557aab0d)

--- src/solidity/Web3Scripts/PickTestNet.js
+++ src/solidity/Web3Scripts/PickTestNet.js
@@ -1,14 +1,17 @@
 let kovan=false;
-let infuraKovan="";
-let infuraRinkeby="";
-let idKovan=42;
-let idRinkeby=4;
-let publicAddress='';
-let privateAddress='';
-let result = 0;
+//let infuraKovan="";
+//let infuraRinkeby="";
+const idKovan="42";
+const idRinkeby="4";
+//let publicAddress='';
+//let privateAddress='';
+let past_events = null;
 let web3=null;
 let provider=null;
 let contract=null;
 
-module.exports={kovan,infuraKovan,infuraRinkeby,idKovan,idRinkeby,publicAddress,privateAddress,result
+/*module.exports={kovan,infuraKovan,infuraRinkeby,idKovan,idRinkeby,publicAddress,privateAddress,result
+,web3,provider,contract};*/
+
+module.exports={kovan,idKovan,idRinkeby,past_events
 ,web3,provider,contract};
